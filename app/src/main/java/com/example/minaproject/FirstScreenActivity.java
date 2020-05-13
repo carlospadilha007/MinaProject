445350253e775f92ef1ea8a83b9ea4004953efaa
Carlos Padilha <cpadilha38@outlook.com.br>
2020-05-13 20:51:23 -0300

    Criação das telas: primeira, login e criação de conta

---
+++ app/src/main/java/com/example/minaproject/FirstScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.minaproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_screen);
+    }
+}
