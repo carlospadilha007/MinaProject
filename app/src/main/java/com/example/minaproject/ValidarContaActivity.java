00a840fc8ea6fb409130afd5593acda97378eded
Carlos Padilha <cpadilha38@outlook.com.br>
2020-05-14 21:10:27 -0300

    tela de validação de conta terminada

---
+++ app/src/main/java/com/example/minaproject/ValidarContaActivity.java
@@ -0,0 +1,14 @@
+package com.example.minaproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ValidarContaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validar_conta);
+    }
+}
